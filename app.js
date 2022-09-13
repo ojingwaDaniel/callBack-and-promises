44eb9955aff14d7a2aceebbb4e84c0e7b3559a39
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-13 14:41:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react' ;
+
+
+this.state = {
+  players: [
+    {
+      name: "ter Stergen",
+    },
+    {
+      name: "Kounde",
+    },
+    {
+      name: "Araujo",
+    },
+    {
+      name: "Pedri",
+    },
+    {
+      name: "Gavi",
+    },
+    {
+      name: "Raphinha",
+    },
+    {
+      name: "Dembele",
+    },
+    {
+      name: "Lewandoski",
+    },
+  ],
+};
