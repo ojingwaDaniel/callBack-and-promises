e12677decc365ae3e5f16e9105ac085aa30d0642
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-14 09:50:09 +0100

    callBack example

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-import React, {Component} from 'react' ;
+let btn = document.querySelector('button')
+
+const moveMe = (element,amount,delay)=>{
+  setTimeout(()=>{
+    element.style.transform = `translate(${amount}px)`
 
 
-this.state = {
-  players: [
-    {
-      name: "ter Stergen",
-    },
-    {
-      name: "Kounde",
-    },
-    {
-      name: "Araujo",
-    },
-    {
-      name: "Pedri",
-    },
-    {
-      name: "Gavi",
-    },
-    {
-      name: "Raphinha",
-    },
-    {
-      name: "Dembele",
-    },
-    {
-      name: "Lewandoski",
-    },
-  ],
-};
+  },delay)
+
+}
+moveMe(btn,100,1000)
+moveMe(btn,200,2000)
+moveMe(btn,300,3000)
+moveMe(btn,400,4000)
+moveMe(btn,500,5000)
+moveMe(btn,600,6000)
+moveMe(btn,700,7000)
+moveMe(btn,800,8000)
+moveMe(btn,900,9000)
+moveMe(btn,1000,10000)
+moveMe(btn, 1100, 11000);
